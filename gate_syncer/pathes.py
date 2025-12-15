54b15c47354655668e39a70634b79b4defbc498b
Lobzikfase2 <86695758+Lobzikfase2@users.noreply.github.com>
2025-12-16 02:57:38 +0300

    Update pathes.py

--- gate_syncer/pathes.py
+++ gate_syncer/pathes.py
@@ -3,5 +3,5 @@ from pathlib import Path
 script_path = Path(__file__).absolute().parent
 streamd_dir_path = script_path.parent
 file_path = Path(script_path, "file_with_path")
-proxy_domain_path = Path(script_path, "PROXY_DOMAIN")
+proxy_domain_path = Path(streamd_dir_path, "PROXY_DOMAIN")
 log_file_path = Path(streamd_dir_path, "sync.log")
